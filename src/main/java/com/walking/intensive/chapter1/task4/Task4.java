4f96314730d7d8c720cd46cab51113bdf7babd34
Angelina Khalueva <khalueva.angelina@gmail.com>
2024-11-24 02:07:15 +0300

    Task4 выполнена

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -34,8 +34,27 @@ public class Task4 {
     }
 
     static String solveEquation(double a, double b, double c) {
-        //        Место для вашего кода
+        if (a != 0) {
+            double d = Math.pow(b, 2) - 4 * a * c;
 
-        return null; // Заглушка. При реализации - удалить
+            if (d < 0) {
+                return "Количество решений: 0.";
+            } else if (d == 0) {
+                double x = -b / (2 * a);
+                return "Количество решений: 1. Корень: " +  (x != 0 ? x : Math.abs(x));
+            } else {
+                double x1 = (-b + Math.sqrt(d)) / (2 * a);
+                double x2 = (-b - Math.sqrt(d)) / (2 * a);
+                return "Количество решений: 2. Корни: " + Math.min(x1, x2) + ";" + Math.max(x1, x2);
+            }
+        } else if (b != 0) {
+            double x = -c / b;
+            return "Количество решений: 1. Корень: " +  (x != 0 ? x : Math.abs(x));
+        } else if (c == 0) {
+            return "Бесконечное множество решений.";
+        } else {
+            return "Количество решений: 0.";
+        }
     }
+
 }
