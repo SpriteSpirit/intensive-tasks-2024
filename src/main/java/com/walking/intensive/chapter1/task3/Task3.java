20022764fb4a7ae27b50be00f886d520f0deba4e
Angelina Khalueva <khalueva.angelina@gmail.com>
2024-11-24 14:04:32 +0300

    Внесены правки: условие объединено в один блок.

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -29,10 +29,6 @@ public class Task3 {
     }
 
     static boolean isLeap(int year) {
-        if (year < 0) {
-            return false;
-        }
-
-        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+        return year >= 0 && year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
     }
 }
